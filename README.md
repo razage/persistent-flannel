3707e67dbaea09a51211c90bf4d71550d9935a29
Thomas Hinkle <thomashinkle2393@gmail.com>
2017-07-07 09:24:45 -0700

    added a test and travis support.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Persistent Flannel
+# Persistent Flannel [![Build Status](https://travis-ci.org/razage/persistent-flannel.svg?branch=master)](https://travis-ci.org/razage/persistent-flannel)
---
+++ pf/tests/test_routes.py
@@ -0,0 +1,24 @@
+import os
+from tempfile import mkstemp
+from unittest import TestCase
+
+from pf import app, db
+
+
+class RouteTestCase(TestCase):
+
+    def setUp(self):
+        self.db, app.config['DATABASE'] = mkstemp()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+        with app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        os.close(self.db)
+        os.unlink(app.config['DATABASE'])
+
+    def test_index(self):
+        rv = self.app.get('/')
+        assert rv.status == "200"
