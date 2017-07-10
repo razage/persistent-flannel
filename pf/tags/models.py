99f715dca4789e02681bd38f3e2eb4559775ec26
Thomas Hinkle <thomashinkle2393@gmail.com>
2017-07-09 23:00:10 -0700

    changed tag_type to an Enum.

--- pf/tags/models.py
+++ pf/tags/models.py
@@ -1,12 +1,21 @@
+from enum import Enum
+
+from sqlalchemy_utils.types import ChoiceType
+
 from pf import db
 from pf.models import BaseModel
+
+
+class TagType(Enum):
+    GENERIC = 0
+    GENRE = 1
 
 
 class Tag(BaseModel):
     __tablename__ = "tags"
 
-    name = db.Column(db.String(32), unique=True)
-    tag_type = db.Column(db.String(10))
+    name = db.Column(db.String(32), unique=True, nullable=False)
+    tag_type = db.Column(ChoiceType(TagType, impl=db.Integer()), nullable=False)
 
     games_in_genre = db.relationship('Game', secondary="assoc_game_genres", back_populates="genres")
     games_in_tag = db.relationship('Game', secondary="assoc_game_tags", back_populates="tags")
@@ -17,8 +26,8 @@ class Tag(BaseModel):
 
     @property
     def is_genre(self):
-        return self.tag_type == "Genre"
+        return self.tag_type is TagType.GENRE
 
     @property
     def is_generic(self):
-        return self.tag_type == "Generic"
+        return self.tag_type is TagType.GENERIC
