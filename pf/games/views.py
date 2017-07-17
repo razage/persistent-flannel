253998ae489edd486a092fdd9aaffc2fdd1ac7fe
Thomas Hinkle <thomashinkle2393@gmail.com>
2017-07-17 02:15:42 -0700

    the game view now properly shows a title.

--- pf/games/views.py
+++ pf/games/views.py
@@ -8,4 +8,4 @@ mod = Blueprint("games", __name__, url_prefix="/games")
 @mod.route('/<game_id>')
 def view_game(game_id):
     game = Game.query.get_or_404(game_id)
-    return render_template("games/game_view.html", game=game)
+    return render_template("games/game_view.html", title=game.name, game=game)
